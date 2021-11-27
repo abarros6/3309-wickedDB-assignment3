a8a7d23cbfe94d96cfb1e33b95d9d592b70ddada
Anthony Barros <abarros6@uwo.ca>
2021-11-26 20:43:03 -0500

    fixed std data.More realistic vals, better dist

--- assignment3/src/studentData.js
+++ assignment3/src/studentData.js
@@ -27,15 +27,36 @@ function getName() {
 }
 
 // assuming all students are at or around highschool age 
+let ageCounter = 0;
 function getStudentAge() {
-    let ages = [14, 15, 16, 17, 18, 19];
-    return ages[getRndInteger(0,5)];
+    ageCounter++;
+    switch (ageCounter) {
+        case 1:
+            return getRndInteger(14,15);
+        case 2: 
+            return getRndInteger(15,16);
+        case 3: 
+            return getRndInteger(16,17);
+        case 4:
+            return getRndInteger(17,18);
+    }
 }
 
 // assuming the value is 
+let yearCounter = 0
 function getStudentYear() {
-    let years = [1, 2, 3, 4];
-    return years[getRndInteger(0,3)];
+    yearCounter++ 
+    switch (yearCounter) {
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3: 
+            return 3;
+        case 4:
+            yearCounter = 0
+            return 4;
+    }
 }
 
 // assuming the averages range from 45 to 100
@@ -44,8 +65,10 @@ function getStudentAverage() {
 }
 
 // in ontario a student needs 18 compulsory and 12 elective credits roughly 8 per year or 4 per sem
-function getCreditsToDate(studentYear) {
-    switch (studentYear) {
+let creditsCounter = 0;
+function getCreditsToDate() {
+    creditsCounter++;
+    switch (creditsCounter) {
         case 1:
             return getRndInteger(0,8);
         case 2:
@@ -53,6 +76,7 @@ function getCreditsToDate(studentYear) {
         case 3:
             return getRndInteger(16,24);
         case 4:
+            creditsCounter = 0;
             return getRndInteger(24,32);
     }
 }
@@ -65,9 +89,9 @@ function getNumberOfClasses() {
 // assuming first number represents floor and next two numbers are actual room number. 3 floors
 // lets say 22 rooms per floor and 3 floors
 function getClassroomNo() {
-    let floor1 = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115];
-    let floor2 = [201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215];
-    let floor3 = [301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315];
+    let floor1 = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
+    let floor2 = [201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222];
+    let floor3 = [301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322];
     
     switch(getRndInteger(0,2)) {
         case 0:
