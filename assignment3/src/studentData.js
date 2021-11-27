cef71933b9e9c6cf6c136032c65cc088fa1150f8
swarit <dholakia.swarit@gmail.com>
2021-11-26 20:42:37 -0500

    updated number of classrooms to 22 per floor

--- assignment3/src/studentData.js
+++ assignment3/src/studentData.js
@@ -65,17 +65,17 @@ function getNumberOfClasses() {
 // assuming first number represents floor and next two numbers are actual room number. 3 floors
 // lets say 22 rooms per floor and 3 floors
 function getClassroomNo() {
-    let floor1 = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115];
-    let floor2 = [201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215];
-    let floor3 = [301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315];
+    let floor1 = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
+    let floor2 = [201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222];
+    let floor3 = [301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 216, 317, 318, 319, 320, 321, 322];
     
     switch(getRndInteger(0,2)) {
         case 0:
-            return floor1[getRndInteger(0,14)]
+            return floor1[getRndInteger(0,21)]
         case 1: 
-            return floor2[getRndInteger(0,14)]
+            return floor2[getRndInteger(0,21)]
         case 2:
-            return floor3[getRndInteger(0,14)]
+            return floor3[getRndInteger(0,21)]
     }
 }
 
