c7607f3358962cd494170a761123a432b8d79d0f
Anthony Barros <abarros6@uwo.ca>
2021-11-26 23:09:33 -0500

    getting some usable data

--- assignment3/src/pythonScripts/jsonToTuple.py
+++ assignment3/src/pythonScripts/jsonToTuple.py
@@ -18,15 +18,15 @@ def convertStudent(data):
 def convertClassroom(data):
     f = open(data)
     text = json.load(f)
-    tuples = text["Student"]
-    r = open('StudentTuples.txt', "w")
+    tuples = text["classroom"]
+    r = open('ClassroomTuples.txt', "w")
     r.write(" ")
     r.close()
-    r = open('StudentTuples.txt', "a")
+    r = open('ClassroomTuples.txt', "a")
 
     for x in tuples:
-        r.write(',' + str((x['studentNo'], x['fName'], x['lName'], x['studentYear'], x['studentAverage'],
-                           x['creditsToDate'], x['numberOfClasses'], x['classroomNo'], x['instructorNo'])))
+        r.write(',' + str((x['classId'], x['classroomNo'],
+                x['homeInstructor'], x['classroomQuantity'])))
 
 
 def convertDepartment(data):
@@ -46,29 +46,34 @@ def convertDepartment(data):
 def convertCourseEquipment(data):
     f = open(data)
     text = json.load(f)
-    tuples = text["CourseEquipment"]
+    tuples = text["CoruseEquipment"]
     r = open('CourseEquipmentTuples.txt', "w")
     r.write(" ")
     r.close()
     r = open('CourseEquipmentTuples.txt', "a")
 
     for x in tuples:
-        r.write(',' + str((x['studentNo'], x['fName'], x['lName'], x['studentYear'], x['studentAverage'],
-                           x['creditsToDate'], x['numberOfClasses'], x['classroomNo'], x['instructorNo'])))
+        r.write(
+            ',' + str((x['deviceNo'], x['deviceName'], x['classAssigned'])))
 
 
 def convertInstructor(data):
     f = open(data)
     text = json.load(f)
-    tuples = text["Instructor"]
+    tuples = text["instructor"]
     r = open('InstructorTuples.txt', "w")
     r.write(" ")
     r.close()
     r = open('InstructorTuples.txt', "a")
 
     for x in tuples:
-        r.write(',' + str((x['studentNo'], x['fName'], x['lName'], x['studentYear'], x['studentAverage'],
-                           x['creditsToDate'], x['numberOfClasses'], x['classroomNo'], x['instructorNo'])))
+        r.write(',' + str((x['InstructorId'], x['homeClassroom'])))
 
 
-convertStudent('../Student.json')
+convertStudent('../Students/Student.json')
+
+convertInstructor('../Instructor/instructor.json')
+
+convertCourseEquipment('../CourseEquipment/CoruseEquipment.json')
+
+convertClassroom('../Classroom/classroom.json')
